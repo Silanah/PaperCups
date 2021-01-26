596865219051705d6d131243251f9e0c5f57b50a
Silanah <oktawia.czuczwara90@gmail.com>
2021-01-26 01:51:13 +0100

    finished 2nd section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Materials from './components/Materials';
 
 class App extends Component {
   render() {
     return(
       <>
         <Header />
+        <Materials />
       </>
     )
   }
